c23e8c245b2b7363a210936401ac457820c1d138
Morgatron78 <morgan_cope@hotmail.com>
2026-08-12 09:12:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'kraken-watch-v2.134';
+const CACHE = 'kraken-watch-v2.135';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=2.134',
-  './app.js?v=2.134',
+  './styles.css?v=2.135',
+  './app.js?v=2.135',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
